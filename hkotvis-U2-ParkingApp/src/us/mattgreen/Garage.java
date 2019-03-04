a1a33e22c35699ea6501f22363f3fad0082df395
hkotvis <hkotvis@my.wctc.edu>
2019-03-04 08:41:40 -0600

    Unit 1 Project Complete

--- hkotvis-U2-ParkingApp/src/us/mattgreen/Garage.java
+++ hkotvis-U2-ParkingApp/src/us/mattgreen/Garage.java
@@ -1,17 +1,43 @@
 package us.mattgreen;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Garage {
-    ArrayList<String> checkIns = new ArrayList<>();
-    ArrayList<String> lostTickets = new ArrayList<>();
-    void welcome() {
+   public ArrayList<Integer> checkIns = new ArrayList<>();
+   public ArrayList<Integer> lostTickets = new ArrayList<>();
+
+    public void header(){
+        System.out.println("\nBest Value Parking");
+        System.out.println("--------------------");
+    }
+
+    public void parking() throws FileNotFoundException {
+        Scanner inputCh = new Scanner(new File("Check.txt"));
+        while (inputCh.hasNextInt())
+        {
+            int in = inputCh.nextInt();
+            checkIns.add(in);
+        }
+        inputCh.close();
+        Scanner inputL = new Scanner(new File("Lost.txt"));
+        while (inputL.hasNextInt())
+        {
+            int in = inputL.nextInt();
+            lostTickets.add(in);
+        }
+        inputL.close();
+
+    }
+
+    public void welcome() throws FileNotFoundException {
        header();
         System.out.println("1 - Check In");
         System.out.println("2 - Close Garage");
-        // checkIns.add(0, money);
         Scanner kb = new Scanner(System.in);
         if (kb.nextLine().equals("1")) {
             header();
@@ -23,43 +49,95 @@ public class Garage {
             else{
                 loseTicket();
             }
-
+        }
+        else{
+            closeGarage();
         }
     }
 
-    private void header(){
-        System.out.println("Best Value Parking");
-        System.out.println("--------------------");
-    }
-
-
-    private void checkOut(){
-
+    private void checkOut() throws FileNotFoundException {
         Random rand = new Random();
         // Obtain a number between [7 - 12].
         int timeIn = rand.nextInt(6) + 7;
+        // num between 1 and 11
         int timeOut = rand.nextInt(12) + 1;
         header();
         int hours = (12 - timeIn) + timeOut;
-        System.out.println("Receipt for vehicle ID: ");
+        System.out.println("Receipt for Vehicle ID: " + (rand.nextInt(99) + 1));
         System.out.println("\nParked for "+ ((12 - timeIn) + timeOut) + " hours, from "+ timeIn + " am - " + timeOut + " pm" );
         if(hours <= 3){
             System.out.println("$5.00");
+            checkIns.add(5);
         }
         else if (hours > 3 && hours < 13){
             int pay = hours - 3;
-            int paypay= 5+pay;
+            int paypay= 5 + pay;
             System.out.println("$" + paypay +".00");
+            checkIns.add(paypay);
+
         }
         else{
             System.out.println("$15.00");
+            checkIns.add(15);
         }
 
-    }
-   private void loseTicket(){
-        header();
-        System.out.println("Receipt for vehicle ID: ");
-        System.out.println("\nLost Ticket\n$25.00");
+        setCheckFile();
     }
 
+   private void loseTicket() throws FileNotFoundException{
+        header();
+        Random rand = new Random();
+        System.out.println("Receipt for Vehicle ID: " + (rand.nextInt(99) + 1));
+        System.out.println("\nLost Ticket\n$25.00");
+        lostTickets.add(25);
+        setLostFile();
+    }
+
+
+    private void closeGarage() throws FileNotFoundException {
+        header();
+        System.out.println("Activity to Date\n");
+        System.out.println("$"+ getCheckSum() + " was collected from "+ checkIns.size() + " check ins");
+        System.out.println("$"+ getLostSum() + " was collected from "+ lostTickets.size() + " lost tickets");
+        System.out.println("\n$"+ (getCheckSum() + getLostSum()) + " was collected overall");
+
+        setCheckFile();
+        setLostFile();
+    }
+
+    private int getLostSum() {
+        int lostSum = 0;
+        int i =0;
+        while(i < lostTickets.size()){
+            lostSum = lostSum + lostTickets.get(i);
+            i++;
+        }
+        return lostSum;
+    }
+    private int getCheckSum() {
+        int checkSum = 0;
+        int i =0;
+        while(i < checkIns.size()){
+            checkSum = checkSum + checkIns.get(i);
+            i++;
+        }
+        return checkSum;
+    }
+
+    private void setLostFile() throws FileNotFoundException {
+        PrintWriter lostFile = new PrintWriter("Lost.txt");
+        for (int lost:lostTickets) {
+            lostFile.println(lost);
+        }
+        lostFile.close();
+    }
+
+    private void setCheckFile() throws FileNotFoundException {
+        PrintWriter checkFile = new PrintWriter("Check.txt");
+        for (int checkIn:checkIns) {
+            checkFile.println(checkIn);
+        }
+
+        checkFile.close();
+    }
 }
--- hkotvis-U2-ParkingApp/src/us/mattgreen/Main.java
+++ hkotvis-U2-ParkingApp/src/us/mattgreen/Main.java
@@ -1,36 +1,33 @@
 package us.mattgreen;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
-
-import static java.awt.SystemColor.menu;
 
 public class Main { // No scanners or prints... just calling methods
-
-    public static void main(String[] args)  {
-        Garage g = new Garage();
+    public static void main(String[] args) throws FileNotFoundException {
+       Garage g = new Garage();
+        g.parking();
         g.welcome();
 
-
-
-
-
-
-           //g.printReceipt();
-            // $5 for 3 hours, for each additional hour it's + $1
-            //Can't charge more than $15 unless lost a ticket ($25)
         }
-
-
-
-
-
-
-//The application needs to store all of the tickets in a file.  When the application is opened, that file needs to load all of the tickets in a collection.
-
 
     }
 
+
+
+// ArrayList<Integer> checkIns = new ArrayList<>();
+// ArrayList<String> lostTickets = new ArrayList<>();
+       /* Scanner input = new Scanner(new File("Parking.txt"));
+        while (input.hasNextInt())
+        {
+            int in = input.nextInt();
+            checkIns.add(in);
+            //System.out.println(in);
+        }
+        input.close();
+        checkIns.add(99);
+        System.out.println(checkIns);
+        PrintWriter outputFile = new PrintWriter("Parking.txt");
+        for (int checkIn:checkIns) {
+            outputFile.println(checkIn);
+        }
+        outputFile.close();*/
